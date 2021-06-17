79d1e3e66583ca1b9036bc7f7babb04063383d36
Olly <62193068+OllyKim@users.noreply.github.com>
2021-06-17 23:06:58 +0900

    Update 33_Dealing_with_Tree_Structure.py
    Update dealing with tree structure component.

--- 33_Dealing_with_Tree_Structure.py
+++ 33_Dealing_with_Tree_Structure.py
@@ -1,30 +1,26 @@
-#Dealing with tree data structure in Grasshopper python component.
-
 import rhinoscriptsyntax as rs
-# to handle tree data structure using python, must import Grasshopper.DataTree module.
 import Grasshopper.DataTree as dt
 
-# Extracting numbers of branches from data structure.
-Number_of_branches = x.BranchCount
-# convert_tree list will be used when reorganizing python list to grasshopper tree structured data.
-convert_tree = dt[object]()
+#step 1. Expand tree structure and append the results to python list.
+n_of_branches = x.BranchCount
+gh_to_py_listlist = []
 
-# Step1. Expand tree structure and append the result to python list structure.
-gh_to_py_list = []
+for i in range(n_of_branches):
+    gh_to_py_listlist.append(x.Branch(i)) 
+    # x.Branch(i) = A list in each branch
+    # Append lists to prepared python list
 
-for i in range(Number_of_branches):
-    gh_to_py_list.append(x.Branch(i))
+#step 2. Manipulating data and storing
+new_list = []
+for list in gh_to_py_listlist: 
+    list = [2*i for i in list] #Manipulating original data
+    new_list.append(list)
+        
 
-print gh_to_py_list
-
-# Step2. Manipulating data using python syntax and structure.
-for list in gh_to_py_list:
-    for i in list:
-        print i
-
-# Step3. Reorgazning python data to Grasshopper tree structure.
+#step 3. Reorganizing python list to Grasshopper tree structure.
+convert_list = dt[object]()
 paths = x.Paths
-for i in range(Number_of_branches):
-    convert_tree.AddRange(gh_to_py_list[i], paths[i])
-
-a = convert_tree
+for i in range(n_of_branches):
+    convert_list.AddRange(new_list[i], paths[i])
+    
+a = convert_list
