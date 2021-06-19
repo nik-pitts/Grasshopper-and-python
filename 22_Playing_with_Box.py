9b9bc3744bb6e0fc0669b48e12076e7db357c0ba
Olly <62193068+OllyKim@users.noreply.github.com>
2021-06-19 23:54:32 +0900

    Update 22_Playing_with_Box.py
    Explode box algorithm description.

--- 22_Playing_with_Box.py
+++ 22_Playing_with_Box.py
@@ -1,8 +1,14 @@
 import rhinoscriptsyntax as rs
 import ghpythonlib.treehelpers as th
 
+#This code is to explode original cube boxes.
 
+#1. Turning box into Rhino object
 raw_bb_base = th.tree_to_list(b,retrieve_base=None)
 bb_base = rs.coercebrep(raw_bb_base)
 
-print bb_base
+#2. Finding cetroid of Boxes
+
+#3. Scale every centroid points
+
+#4. Move original geometries to centroid points
