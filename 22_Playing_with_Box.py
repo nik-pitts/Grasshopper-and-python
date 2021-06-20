bce749e8dbeda7ecfc7db0244cd2f1a89d74550f
Olly <62193068+OllyKim@users.noreply.github.com>
2021-06-20 17:35:00 +0900

    Update 22_Playing_with_Box.py
    The centroid of cube boxes is extracted.
    The next step shall be scaling that centroid and append existing boxes to newly scaled points.

--- 22_Playing_with_Box.py
+++ 22_Playing_with_Box.py
@@ -2,23 +2,16 @@ import rhinoscriptsyntax as rs
 import ghpythonlib.treehelpers as th
 import Rhino
 
-#This code is to explode original cube boxes into bigger scale.
 
 input_tree = th.tree_to_list(x,retrieve_base=None)
-
-#1. Turning box into Rhino object
 
 geo_list = []
 
 for list_list in input_tree:
     for list in list_list:
         for i in list:
-            geo = rs.coercesurface(i)
-            geo_list.append(i)
-
-print len(geo_list)
-
-#2. Finding cetroid of Boxes
+            coerce_ob = rs.coercebrep(i)
+            geo_list.append(coerce_ob)
 
 centroid_list = []
 
@@ -26,3 +19,6 @@ for i in geo_list:
     geo_analysis = Rhino.Geometry.AreaMassProperties.Compute(i)
     centroid = geo_analysis.Centroid
     centroid_list.append(centroid)
+
+print len(centroid_list)
+
