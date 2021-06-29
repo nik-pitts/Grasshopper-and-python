f41f07de7b09bc1cf75dbe1cec4888e9a1cd9bd8
Olly <62193068+OllyKim@users.noreply.github.com>
2021-06-30 00:31:32 +0900

    Update 23_Car_On_the_Hill.py

--- 23_Car_On_the_Hill.py
+++ 23_Car_On_the_Hill.py
@@ -2,6 +2,8 @@ hill = rs.coercecurve(x)
 # reparametrize hill crv 0 to 1.
 hill.Domain = rh.Geometry.Interval(0.00,1.00)
 
-eval = rs.EvaluateCurve(hill, param)
+eval1 = rs.EvaluateCurve(hill, param)
+eval2 = rs.EvaluateCurve(hill, param + wheel_distance)
 
-a = eval
+a = eval1
+b = eval2
